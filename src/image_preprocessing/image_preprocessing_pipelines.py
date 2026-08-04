e0ba055429242f874add4173c7eb8884ecfae913
Avneet Kaur <63954648+Avneetkaur16@users.noreply.github.com>
2026-08-05 03:14:53 +0530

    Update image_preprocessing_pipelines.py

--- src/image_preprocessing/image_preprocessing_pipelines.py
+++ src/image_preprocessing/image_preprocessing_pipelines.py
@@ -7,7 +7,7 @@ def baseline_preprocessing(image_path):
   # Read, resize and normalize image
   image = cv2.imread(image_path)
   image = resize_image(image)
-  #image = normalize_image(image)
+  image = normalize_image(image)
   image = image.astype(np.float32)
   return image
 
