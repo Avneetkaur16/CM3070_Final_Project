2998e148f856b594c9e7caed5216ec58488c186a
Avneetkaur16 <63954648+Avneetkaur16@users.noreply.github.com>
2026-08-05 01:33:18 +0530

    Updated VGG16 layers and optimizer learning rate

--- src/models/view_independent/vgg16CNN.py
+++ src/models/view_independent/vgg16CNN.py
@@ -8,6 +8,7 @@ def create_vgg16_model():
   input = layers.Input(shape=(224, 224, 3))
   x = vgg16(input, training=False)
   x = layers.GlobalAveragePooling2D()(x)
+  x = layers.Dense(256, activation='relu')(x)
   x = layers.Dropout(0.3)(x)
   output = layers.Dense(1, activation='sigmoid')(x)
 
