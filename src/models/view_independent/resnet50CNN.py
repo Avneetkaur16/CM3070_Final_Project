fccd97a94ee37928a4c5478e699960877d7c963a
Avneetkaur16 <63954648+Avneetkaur16@users.noreply.github.com>
2026-08-05 14:14:16 +0530

    Corrected Pre-trained models and image preprocessing pipelines

--- src/models/view_independent/resnet50CNN.py
+++ src/models/view_independent/resnet50CNN.py
@@ -1,13 +1,26 @@
+import tensorflow as tf
 from tensorflow.keras import layers, Model
 from tensorflow.keras.applications import ResNet50
 
 def create_resnet50_model():
+  # Load the ResNet50 model with ImageNet weights
   resnet50 = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+  # Freeze the layers
   resnet50.trainable = False
 
+  # Input layer
   input = layers.Input(shape=(224, 224, 3))
+
+  # ResNet50-specific image preprocessing
+  x = tf.keras.applications.resnet50.preprocess_input(input)
+
+  # Train the ResNet50 model using imagenet weights
   x = resnet50(input, training=False)
+
+  # Classification
   x = layers.GlobalAveragePooling2D()(x)
+  x = layers.Dense(256, activation='relu')(x)
   x = layers.Dropout(0.3)(x)
   output = layers.Dense(1, activation='sigmoid')(x)
 
--- src/models/view_independent/vgg16CNN.py
+++ src/models/view_independent/vgg16CNN.py
@@ -1,12 +1,24 @@
+import tensorflow as tf
 from tensorflow.keras import layers, Model
 from tensorflow.keras.applications import VGG16
 
 def create_vgg16_model():
+  # Load the VGG16 model with ImageNet weights
   vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+  # Freeze the layers
   vgg16.trainable = False
 
+  # Input layer
   input = layers.Input(shape=(224, 224, 3))
+
+  # VGG16-specific image preprocessing
+  x = tf.keras.applications.vgg16.preprocess_input(input)
+
+  # Train the VGG16 model using imagenet weights
   x = vgg16(input, training=False)
+
+  # Classification
   x = layers.GlobalAveragePooling2D()(x)
   x = layers.Dense(256, activation='relu')(x)
   x = layers.Dropout(0.3)(x)
