b6e856eb902158fe3c236c9140f1e44225a43e7e
Avneetkaur16 <63954648+Avneetkaur16@users.noreply.github.com>
2026-08-31 21:55:47 +0530

    Learning rate and optimization steps for temperature scaling adjusted for faster convergence

--- src/calibration_scores_functions.py
+++ src/calibration_scores_functions.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from src.constants import TEMPERATURE_OPTIMIZATION_STEPS
+from src.constants import TEMPERATURE_OPTIMIZATION_STEPS, TEMPERATURE_OPTIMIZATION_LEARNING_RATE
 
 # Function to calculate Negative Log-Like Loss for temperature scaling
 def compute_nll(temperature, original_logits, true_labels):
@@ -12,8 +12,8 @@ def compute_nll(temperature, original_logits, true_labels):
 
 # Function to perform temperature scaling
 def temperature_scaling(original_logits, true_labels, temperature):
-  # Define an optimizer for temperature optimization with learning rate 1e-4
-  temp_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+  # Define an optimizer for temperature optimization with learning rate 1e-2
+  temp_optimizer = tf.keras.optimizers.Adam(learning_rate=TEMPERATURE_OPTIMIZATION_LEARNING_RATE)
 
   for i in range(TEMPERATURE_OPTIMIZATION_STEPS):
     # Record all computations of NLL loss
