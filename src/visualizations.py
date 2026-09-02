fd30607545e0e50ff83ee8e7f846ec69452cd146
Avneetkaur16 <63954648+Avneetkaur16@users.noreply.github.com>
2026-09-02 18:59:43 +0530

    Computational cost grouped graph

--- src/visualizations.py
+++ src/visualizations.py
@@ -71,45 +71,15 @@ def plot_grouped_bar_chart_per_metric_final(grouped_df, legend_list, metric_name
 
 # COMPUTATIONAL COSTS GRAPHS
 
-# Bar chart for performance metrics for a given model
-def plot_metrics_bar_chart(eval_metrics, model_name, preprocessor):
-    auc = eval_metrics['auc']
-    sensitivity = eval_metrics['recall']
-    precision = eval_metrics['precision']
-    f1 = eval_metrics['f1_score']
-    accuracy = eval_metrics['accuracy']
-    specificity = eval_metrics['specificity']
+# Grouped models horizontal bar chart for final configurations and given computational metric
+def plot_computational_grouped_bar_chart_per_metric_final(grouped_df, legend_list, metric_name):
+    grouped_df.plot(x='model', y=legend_list, kind='barh', figsize=(6, 10), width=0.5)
+    
+    plt.title(f"{metric_name}-per model for final configuration grouped bar chart")
+    plt.xlabel(f"Model", fontsize=12)
+    plt.ylabel(f"{metric_name}", fontsize=12)
+    plt.xticks(rotation=0)
 
-    metrics = ['AUC', 'Sensitivity', 'Specificity', 'Precision', 'F1 Score', 'Accuracy']
-    metric_values = [auc, sensitivity, specificity, precision, f1, accuracy]
-
-    plt.bar(metrics, metric_values)
-    plt.title(f"{model_name} with {preprocessor}: Performance Metrics")
-    plt.xlabel('Metrics')
-    plt.ylabel('Score')
-    plt.show()
-
-# Horizontal bar chart for training time of all image preprocessing pipelines for a model
-def plot_training_time(training_times, model_name):
-    models = list(training_times.keys())
-    training_time_values = list(training_times.values())
-
-    plt.barh(models, training_time_values)
-    plt.title(f"Training times for pipelines in {model_name}")
-    plt.ylabel('Image Preprocessing Pipeline')
-    plt.xlabel('Training Time (minutes)')
-    plt.show()
-
-# Horizontal bar chart for inference time of all image preprocessing pipelines for a model
-def plot_inference_time(prediction_times, model_name, image_samples):
-    models = list(prediction_times.keys())
-    inference_time_values = list(prediction_times.values())
-
-    for i in range(len(inference_time_values)):
-      inference_time_values[i] = inference_time_values[i] / float(image_samples)
-
-    plt.barh(models, inference_time_values)
-    plt.title(f"Inference times for pipelines in {model_name}")
-    plt.ylabel('Image Preprocessing Pipeline')
-    plt.xlabel('Inference Time (ms)')
+    plt.grid(axis='y', linestyle='-', alpha=0.6)
+    plt.tight_layout()
     plt.show()
