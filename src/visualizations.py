1034fe260be5dcec6fc02889b751519c83259af6
Avneetkaur16 <63954648+Avneetkaur16@users.noreply.github.com>
2026-09-05 06:09:33 +0530

    Removed redundant notebooks and code

--- src/visualizations.py
+++ src/visualizations.py
@@ -17,22 +17,6 @@ def plot_training_validation_loss(history, model_name, experiment_variable):
     plt.legend()
     plt.show()
 
-# Function to plot training-validation accuracy for a given model
-def plot_training_validation_accuracy(history, model_name, experiment_variable):
-    model_history = history.history
-    training_accuracy = model_history['accuracy']
-    validation_accuracy = model_history['val_accuracy']
-    epochs = range(1, len(training_accuracy) + 1)
-
-    plt.figure(figsize=(5, 5))
-    plt.plot(epochs, training_accuracy, label='Training Accuracy')
-    plt.plot(epochs, validation_accuracy, label='Validation Accuracy')
-    plt.title(f"{model_name} with {experiment_variable}: Training and Validation Accuracy Curve")
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    plt.show()
-
 # Confusion matrix for a given model
 def plot_confusion_matrix(true_pathology, predicted_pathology, model_name, experiment_variable):
     cm = confusion_matrix(true_pathology, predicted_pathology)
@@ -46,7 +30,7 @@ def plot_confusion_matrix(true_pathology, predicted_pathology, model_name, exper
 def plot_grouped_bar_chart_per_metric(grouped_df, legend_list, metric_name):
     grouped_df.plot(x='model', y=legend_list, kind='bar', figsize=(12, 6), width=0.7)
 
-    plt.title(f"{metric_name}-per model and experiment grouped bar chart")
+    plt.title(f"{metric_name}-per model grouped bar chart")
     plt.xlabel(f"Model", fontsize=12)
     plt.ylabel(f"{metric_name}", fontsize=12)
     plt.xticks(rotation=0)
@@ -54,20 +38,6 @@ def plot_grouped_bar_chart_per_metric(grouped_df, legend_list, metric_name):
     plt.grid(axis='y', linestyle='-', alpha=0.6)
     plt.tight_layout()
     plt.show()
-
-# Grouped models bar chart for final configurations and given metric
-def plot_grouped_bar_chart_per_metric_final(grouped_df, legend_list, metric_name):
-    grouped_df.plot(x='model', y=legend_list, kind='bar', figsize=(14, 6), width=0.5)
-    
-    plt.title(f"{metric_name}-per model for final configuration grouped bar chart")
-    plt.xlabel(f"Model", fontsize=12)
-    plt.ylabel(f"{metric_name}", fontsize=12)
-    plt.xticks(rotation=0)
-
-    plt.grid(axis='y', linestyle='-', alpha=0.6)
-    plt.tight_layout()
-    plt.show()
-
 
 # COMPUTATIONAL COSTS GRAPHS
 
